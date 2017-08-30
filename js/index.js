95dafc30361f65bb08195cd0aed54dec988abf07
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-30 17:36:36 -0500

    Updates calculator logic with eval

--- js/index.js
+++ js/index.js
@@ -1,14 +1,34 @@
+function cleanText(dirtyText) {
+  // replace X' and division signs with standard operand symbols
+  let cleanText = dirtyText.replace(/x/g, '*').replace(/÷/g, '/');
+  return cleanText;
+}
+
+function textContainsOperand(text) {
+  // console.log(text);
+  return text.match(/[^0123456789C]/g);
+}
+
 $( '.buttons >*:not(#equals)' ).click(function(e) {
+  let expression = $('#screen').text();
   let buttonText = e.target.innerText;
 
-  $('#screen').append(buttonText);
+  // if operand is present and operand is clicked, evaluate then append
+  // if operand is not present, and button is C, then clear, ELSE append what was clicked
+  if(textContainsOperand(expression) && textContainsOperand(buttonText)) {
+    $('#screen').text(eval(cleanText(expression)));
+    $('#screen').append(buttonText);
 
-  if (buttonText === 'C') {
+  } else if (buttonText === 'C') {
     $('#screen').text('');
+
+  } else {
+    $('#screen').append(buttonText);
   }
 });
 
 $('#equals').click(function() {
   let expression = $('#screen').text();
-  $('#screen').text(eval(expression));
+
+  $('#screen').text(eval(cleanText(expression)));
 });
