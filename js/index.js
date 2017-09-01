dbad8618addf26943bfaa6842a8278c6f9613aaa
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-01 09:56:39 -0500

    Adds keypress event for handling esc button

--- js/index.js
+++ js/index.js
@@ -1,72 +1,83 @@
-// replace x's and division signs in a string standard operand symbols
-function cleanText(dirtyText) {
-  let cleanText = dirtyText.replace(/x/g, '*').replace(/÷/g, '/');
-  return cleanText;
-}
-
-// check if 'invalid' text is included in a string
-function textContainsOperand(text) {
-  return text.match(/[^0123456789C]/g);
-}
-
-// calculator logic
-function handleInput(char) {
-  let expression = $('#screen').text();
-
-  // if equals sign is clicked or typed, clean and evalute expression
-  if (char === '=') {
-    return $('#screen').text(eval(cleanText(expression)));
+(function(){
+  // replace x's and division signs in a string standard operand symbols
+  function cleanText(dirtyText) {
+    let cleanText = dirtyText.replace(/x/g, '*').replace(/÷/g, '/');
+    return cleanText;
   }
 
-  // if operand is present and operand is clicked, evaluate then append
-  // if operand is not present, and button is C, then clear, ELSE append what was clicked
-  if(textContainsOperand(expression) && textContainsOperand(char)) {
-    $('#screen').text(eval(cleanText(expression)));
-    $('#screen').append(char);
-  } else if (char === 'C') {
-    $('#screen').text('');
-  } else {
-    $('#screen').append(char);
+  // check if 'invalid' text is included in a string
+  function textContainsOperand(text) {
+    return text.match(/[^0123456789C]/g);
   }
-}
 
-// returns values based on which key was pressed by user
-function translateKeyPress(key) {
-  if (key >= 48 && key <= 57 ) {
-    return (key - 48).toString();
+  // calculator logic
+  function handleInput(char) {
+    let expression = $('#screen').text();
+
+    // if equals sign is clicked or typed, clean and evalute expression
+    if (char === '=') {
+      return $('#screen').text(eval(cleanText(expression)));
+    }
+
+    // handles similar to old style calculator - multiple operands do not appear in line
+    // if operand is present and operand is clicked, evaluate then append
+    // if operand is not present, and button is C, then clear, ELSE append what was clicked
+    if(textContainsOperand(expression) && textContainsOperand(char)) {
+      $('#screen').text(eval(cleanText(expression)));
+      $('#screen').append(char);
+    } else if (char === 'C') {
+      $('#screen').text('');
+    } else {
+      $('#screen').append(char);
+    }
   }
-  switch(key) {
-    case 43:
-      return '+';
-    case 47:
-      return '÷';
-    // lowercase 'x' and asterisk '*'
-    case 42:
-    case 120:
-      return 'x';
-    case 45:
-      return '-';
-    // equals sign AND return
-    case 61:
-    case 13:
-      return '=';
-    default:
-    return null;
+
+  // returns values based on which key was pressed by user
+  function translateKeyPress(key) {
+    if (parseInt(key) || key ==='0') {
+      return key;
+    }
+    switch(key) {
+      case '+':
+        return '+';
+      case '/':
+        return '÷';
+      case '*':
+      case 'x':
+        return 'x';
+      case 45:
+        return '-';
+      case 'Enter':
+      case '=':
+        return '=';
+      case 'c':
+      case 'C':
+        return 'C';
+      default:
+      return null;
+    }
   }
-}
 
-// event handlers
-// clicks on the calculator
-$( '.buttons' ).click(function(e) {
-  let buttonText = e.target.innerText;
+  // event handlers
+  // clicks on the calculator
+  $( '.buttons' ).click(function(e) {
+    let buttonText = e.target.innerText;
 
-  handleInput(buttonText);
-});
+    handleInput(buttonText);
+  });
 
-// keyboard entry
-$('body').keypress((e) => {
-  let pressedKey = e.which;
-  if (translateKeyPress(pressedKey)) {
-    handleInput(translateKeyPress(pressedKey));
-  }
-});
+  // keyboard entry
+  $('body').keydown((e) => {
+    let pressedKey = e.key;
+    if (pressedKey === 'Escape') {
+      handleInput('C');
+    }
+  });
+
+  $('body').keypress((e) => {
+    let pressedKey = e.key;
+    if (translateKeyPress(pressedKey)) {
+      handleInput(translateKeyPress(pressedKey));
+    }
+  });
+})()
